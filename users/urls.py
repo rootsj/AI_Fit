600916d0409e6c701be944ab68816810c5c05f78
rootsj <hwa9981@naver.com>
2020-12-17 16:06:16 +0900

    feature useredit urls add
    유저정보 수정 페이지 url 추가

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("facelogin", views.face_login_view, name="face_login"),
     path("logout", views.logout_view, name="logout"),
     path("signup", views.signup_view, name="signup"),
+    path("useredit", views.user_edit, name='useredit'),
     path("sports/<str:what_kind>", views.sports_view, name="sports"),
     path("sports/counts/<int:count_result>/<str:what_kind>", views.count_view, name="counts")
 ]
